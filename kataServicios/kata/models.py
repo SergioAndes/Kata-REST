a93d64016cd805950b5e0a47f974298c066cdbf3
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-13 22:05:35 -0500

    todos los modelos

--- kataServicios/kata/models.py
+++ kataServicios/kata/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    usuario = models.CharField(max_length=200)
+    foto = models.CharField(max_length=200)
+    perfilProfesional = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Portafiolio(models.Model):
+    nombrePortafolio = models.CharField(max_length=200)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    esPublico = models.BooleanField()
+
+    def __str__(self):
+        return self.nombrePortafolio
+
+
+class Imagen(models.Model):
+    titulo = models.CharField(max_length=200)
+    enlace = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    tipoDeArchivo = models.CharField(max_length=200)
+    esPublica = models.BooleanField()
+    portafolio = models.ForeignKey(Portafiolio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
--- kataServicios/kata/views.py
+++ kataServicios/kata/views.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -7,13 +6,3 @@ from django.core import serializers
 import json
 
 # Create your views here.
-class Pregunta(models.Model):
-    nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)
-    usuario = models.CharField(max_length=200)
-    foto = models.CharField(max_length=200)
-    perfilProfesional = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.nombre
