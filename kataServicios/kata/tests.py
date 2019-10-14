2ac64102e003125b6139d75432cf375d1b68c683
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-13 22:33:35 -0500

    servicio que lista portafilios (ROJO)

--- kataServicios/kata/tests.py
+++ kataServicios/kata/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase, Client
 
+
 # Create your tests here.
 
 class KataTestCase(TestCase):
+
+    def test_servicio_list_status(self):
+        url = '/kata/portafolio/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
